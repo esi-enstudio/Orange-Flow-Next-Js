009e71808786dedd9917c0131a8b3bcedf48b96d
Sadekin Islam <sadekinislam6@gmail.com>
2026-06-02 18:47:50 +0600

    update

--- backend/main.py
+++ backend/main.py
@@ -83,7 +83,8 @@ app.include_router(webhook_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://orange-flow-next-js.vercel.app"],
+    allow_origin_regex=r"https://orange-flow-next-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
